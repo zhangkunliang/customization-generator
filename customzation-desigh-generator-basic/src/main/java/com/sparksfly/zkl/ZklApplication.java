efa72ab2c98ea0a1504b1bde2f9fb852b1430377
zhangkunliang <1005789053@qq.com>
2023-12-21 15:47:19 +0800

    项目初始化

---
+++ customzation-desigh-generator-basic/src/main/java/com/sparksfly/zkl/ZklApplication.java
@@ -0,0 +1,13 @@
+package com.sparksfly.zkl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZklApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZklApplication.class, args);
+    }
+
+}
